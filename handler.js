ccccd75f468f90e60f259c8a9dde1b2d69046240
serdi12 <109755111+serdi12@users.noreply.github.com>
2022-07-22 17:50:44 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -30,14 +30,14 @@ module.exports = {
         global.namabot = conn.user.name
         global.packname = global.namabot
         global.author = global.data.owner
-        //global.author = '𝐉𝐚𝐫𝐨𝐭'
+        //global.author = '𝚒𝚢𝚊𝚗Ⓒ︎'
         global.wm2 = global.data.namabot + ' ' + global.data.owner
-        global.wm = '© 𝐀𝐥𝐩𝐢 𝐁𝐨𝐭'
+        global.wm = '© 𝚁𝙲𝚃𝙸 𝙱𝙾𝚃'
         global.colong1 = 'Ciss 📸'
-        global.colong2 = '𝐀𝐥𝐩𝐢 𝑩𝒐𝒕'
+        global.colong2 = '𝚁𝙲𝚃𝙸 𝙱𝙾𝚃'
         global.kontak2 = [
-        ['6285850539404', '𝐉𝐚𝐫𝐨𝐭', '𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓 𝑩𝒐𝒕', 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ', true],
-        ['6285713137903', await this.getName('6285713137903@s.whatsapp.net'), '𝑷𝒂𝒓𝒕𝒏𝒆𝒓 𝐀𝐥𝐩𝐢 𝑩𝒐𝒕', 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ', true], 
+        ['6289516947204', '𝚒𝚢𝚊𝚗Ⓒ︎', '𝑫𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓 𝑩𝒐𝒕', 'https://saweria.co/IyanMd', true],
+        ['6289516947204', await this.getName('6289516947204@s.whatsapp.net'), '𝑷𝒂𝒓𝒕𝒏𝒆𝒓 𝚁𝙲𝚃𝙸 𝙱𝙾𝚃', 'https://saweria.co/IyanMd', true], 
         ]
         global.bg = await (await fetch(img)).buffer()
         global.time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
